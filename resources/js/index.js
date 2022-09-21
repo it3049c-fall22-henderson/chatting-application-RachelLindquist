086b950611e9bf1a4918645c2d1d799b6c9d32ad
RachelLindquist <lindqurm@mail.uc.edu>
2022-09-21 13:28:36 -0400

    updateMessages finished

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,5 @@
+const MILLISECONDS_IN_TEN_SECONDS = 10000;
+
 const nameInput = document.getElementById("my-name-input");
 const myMessage = document.getElementById("my-message");
 const sendButton = document.getElementById("send-button");
@@ -10,22 +12,48 @@ function fetchMessages() {
   return fetch(messagesURL).then( response => response.json())
 }
 
+function formatMessage(message, myNameInput) {
+  const time = new Date(message.timestamp);
+  const formattedTime = `${time.getHours()}:${time.getMinutes()}`;
+
+  if (myNameInput === message.sender) {
+      return `
+      <div class="mine messages">
+          <div class="message">
+              ${message.text}
+          </div>
+          <div class="sender-info">
+              ${formattedTime}
+          </div>
+      </div>
+      `
+  } else {
+      return `
+          <div class="yours messages">
+              <div class="message">
+                  ${message.text}
+              </div>
+              <div class="sender-info">
+                  ${message.sender} ${formattedTime}
+              </div>
+          </div>
+      `
+  }
+}
+
 async function updateMessages() {
   // Fetch Messages
   const messages = await fetchMessages();
-  console.log(messages)
-
   // Loop over the messages. Inside the loop we will
       // get each message
       // format it
       // add it to the chatbox
+  let formattedMessages = "";
+  messages.forEach(message => {
+    formattedMessages += formatMessage(message, nameInput.value);
+  });
+  chatBox.innerHTML = formattedMessages;
 }
 
-
-
-function updateMessagesInChatBox(){
-  fetchMessages()
-  formatMessages()
-  updateChatBox()
-}
+setInterval(updateMessages, MILLISECONDS_IN_TEN_SECONDS);
 
